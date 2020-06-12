cd44ecd81b8b1d8ad7486a39227afb80b8c0ed58
Zack <mwl@live.com>
2020-06-12 17:13:44 +0800

    💞 添加路由

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"gova/router"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Routers() *gin.Engine {
+	var Router = gin.Default()
+	// Router.Use(middleware.LoadTls())  // 打开就能玩https了
+	// 跨域
+	Router.Use(middleware.Cors())
+	// 方便统一添加路由组前缀 多服务器上线使用
+	ApiGroup := Router.Group("")
+	router.InitApiRouter(ApiGroup) // 注册功能api路由
+	return Router
+}
+
 func main() {
 
 }
