ec908c238bcdec668ad92dfddd389cc96eeda42a
Zack <mwl@live.com>
2020-06-12 19:06:40 +0800

    初始化vue

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func Routers() *gin.Engine {
 	var Router = gin.Default()
 	// Router.Use(middleware.LoadTls())  // 打开就能玩https了
 	// 跨域
-	Router.Use(middleware.Cors())
+	// Router.Use(middleware.Cors())
 	// 方便统一添加路由组前缀 多服务器上线使用
 	ApiGroup := Router.Group("")
 	router.InitApiRouter(ApiGroup) // 注册功能api路由
